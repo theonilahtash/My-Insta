6bf348848cca7d9ed254bf1e43399f95d8d7e051
Theonilah Owali <theonilahtash@gmail.com>
2018-12-15 16:50:15 +0300

    add a return string representation of our model

--- stazone/models.py
+++ stazone/models.py
@@ -5,9 +5,13 @@ from django.db import models
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=30)
-    post = HTMLField()
+    # post = HTMLField()
     caption = models.TextField(max_length=50)
-    profile = models.ForeignKey(User,on_delete=models.CASCADE)
+    # profile = models.ForeignKey(User,on_delete=models.CASCADE)
     likes = models.PositiveIntegerField()
     comments = models.CharField(max_length=60)
     profile_image = models.ImageField(upload_to='profiles/',blank=True)
+
+
+    def __str__(self):
+        return self.caption
