1e879431125233cd128de285d91f615cfc06cc5d
Theonilah Owali <theonilahtash@gmail.com>
2018-12-15 17:56:28 +0300

    added a static route the references the location of uploaded files

--- stazone/urls.py
+++ stazone/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
@@ -7,3 +9,5 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results')
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
