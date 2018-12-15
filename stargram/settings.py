3bd1e57bb606fdbdc9726520043cd47dc496ce44
Theonilah Owali <theonilahtash@gmail.com>
2018-12-15 14:33:25 +0300

    added a navigation bar

--- stargram/settings.py
+++ stargram/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'stazone',
-    'bootstrap4'
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
