287b20b2c4a1da49b9159ec09f0fbd9575bd3e94
Theonilah Owali <theonilahtash@gmail.com>
2018-12-15 14:03:43 +0300

    added a view function that will render today stazone

--- stazone/views.py
+++ stazone/views.py
@@ -5,3 +5,7 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+def stazone_today(request):
+    return render(request,'all-stazone/today-stazone.html')
