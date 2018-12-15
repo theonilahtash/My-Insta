6aec7c831ec19dfc0b7e6ecf15be6bc63358720e
Theonilah Owali <theonilahtash@gmail.com>
2018-12-15 17:29:59 +0300

    added a view function for the search form

--- stazone/views.py
+++ stazone/views.py
@@ -11,3 +11,15 @@ def stazone_today(request):
     images = Image.objects.all()
     print(images)
     return render(request,'all-stazone/today-stazone.html',{"images":images})
+
+def search_results(request):
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("pofile")
+        searched_profiles = Profile.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request,'all-insta/search.html',{"message":message,"profiles": searched_profiles})
+
+    else:
+        message = "Nothing searched"
+        return render(request,'all-insta/search.html',{"message":message})
