d76e0f6dc84464ff83a7174993bc62b6577683af
Theonilah Owali <theonilahtash@gmail.com>
2018-12-16 11:41:09 +0300

    create a new urlconf for view

--- stazone/urls.py
+++ stazone/urls.py
@@ -7,7 +7,9 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     # url('^today/$',views.images,name='welcome'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^profile/(\d+)',views.profile,name='profile')
+    url(r'^profile/(\d+)',views.profile,name='profile'),
+    url(r'^new/image$', views.new_image,name='new_image')
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
