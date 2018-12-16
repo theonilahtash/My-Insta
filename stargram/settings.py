72a91a4c109ab17c751f1a5a986203be8c70ae43
Theonilah Owali <theonilahtash@gmail.com>
2018-12-16 11:26:13 +0300

    added a htmlfield model from the tinymce package

--- stargram/settings.py
+++ stargram/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'stazone',
     'bootstrap4',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- stazone/models.py
+++ stazone/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=30)
-    # post = HTMLField()
+    post = HTMLField()
     caption = models.TextField(max_length=50)
-    profile = models.ForeignKey(User,on_delete=models.CASCADE)
+    # profile = models.ForeignKey(User,on_delete=models.CASCADE)
     likes = models.PositiveIntegerField()
     comments = models.CharField(max_length=60)
-    profile_image = models.ImageField(upload_to='profiles/',blank=True)
+    # profile_image = models.ImageField(upload_to='profiles/',blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
 
 
